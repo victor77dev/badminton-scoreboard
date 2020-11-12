49b209741fd798d37bde1c599cc29306f21d9645
victor77dev <victor77.dev@gmail.com>
2020-11-12 23:53:47 +0100

    Update indentation

--- src/features/court/index.jsx
+++ src/features/court/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 /** @type {React.FC} */
 export const Court = () => {
-    return (
-        <div>
-            Court
-        </div>
-    );
+  return (
+    <div>
+      Court
+    </div>
+  );
 };
 
--- src/pages/court/index.jsx
+++ src/pages/court/index.jsx
@@ -4,9 +4,9 @@ import { Court } from 'features/court';
 
 /** @type {React.FC} */
 export const CourtPage = () => {
-    return (
-        <Fragment>
-            <Court />
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <Court />
+    </Fragment>
+  );
 };
