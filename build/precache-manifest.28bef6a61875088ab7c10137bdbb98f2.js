56e7a966341841aa42d55f1dd495583ccdc3f346
victor77dev <victor77.dev@gmail.com>
2020-12-25 18:17:57 +0100

    test

---
+++ build/precache-manifest.28bef6a61875088ab7c10137bdbb98f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32861b3880912d7b86ab87bd95f2bbd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a7836790a1a6a7ba8e",
+    "url": "/static/css/main.6a059b00.chunk.css"
+  },
+  {
+    "revision": "4c4029d17fd02bc288dc",
+    "url": "/static/js/2.8e3cff09.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.8e3cff09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af52bbea6ef2c363fd11",
+    "url": "/static/js/3.e7fab494.chunk.js"
+  },
+  {
+    "revision": "d5a7836790a1a6a7ba8e",
+    "url": "/static/js/main.5a05d972.chunk.js"
+  },
+  {
+    "revision": "98275fd5b829605480da",
+    "url": "/static/js/runtime-main.89fa6e6b.js"
+  },
+  {
+    "revision": "0664ab9880baa5a628da288b19fbfe82",
+    "url": "/static/media/sub-court.0664ab98.svg"
+  }
+]);
