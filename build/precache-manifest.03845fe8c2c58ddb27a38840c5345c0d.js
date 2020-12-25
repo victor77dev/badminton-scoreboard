e07eb3edc4f6465e4e3f4b2c944ec3dc0312c993
victor77dev <victor77.dev@gmail.com>
2020-12-25 18:27:10 +0100

    test

---
+++ build/precache-manifest.03845fe8c2c58ddb27a38840c5345c0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c573cb4735586cfb74b7799cbfe05c",
+    "url": "/badminton-scoreboard/build/index.html"
+  },
+  {
+    "revision": "f395b374aa72cc10fc60",
+    "url": "/badminton-scoreboard/build/static/css/main.88b299b6.chunk.css"
+  },
+  {
+    "revision": "4c4029d17fd02bc288dc",
+    "url": "/badminton-scoreboard/build/static/js/2.8e3cff09.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/badminton-scoreboard/build/static/js/2.8e3cff09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af52bbea6ef2c363fd11",
+    "url": "/badminton-scoreboard/build/static/js/3.e7fab494.chunk.js"
+  },
+  {
+    "revision": "f395b374aa72cc10fc60",
+    "url": "/badminton-scoreboard/build/static/js/main.5a05d972.chunk.js"
+  },
+  {
+    "revision": "d36bb755432ef3decf4f",
+    "url": "/badminton-scoreboard/build/static/js/runtime-main.6e272908.js"
+  },
+  {
+    "revision": "0664ab9880baa5a628da288b19fbfe82",
+    "url": "/badminton-scoreboard/build/static/media/sub-court.0664ab98.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.921f9078896ef521e38bd2dadbc0d310.js"
+  "/badminton-scoreboard/build/precache-manifest.03845fe8c2c58ddb27a38840c5345c0d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/badminton-scoreboard/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
