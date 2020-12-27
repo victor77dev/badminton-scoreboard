945a66c47977a2565918d2b3d7b10b26b18c62f3
victor77dev <victor77.dev@gmail.com>
2020-12-27 12:13:40 +0100

    Fix white line gap

--- src/features/court/index.jsx
+++ src/features/court/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { HalfCourt } from './components/half-court';
 import { SubCourt } from './components/sub-court';
@@ -7,6 +7,23 @@ import styles from './index.module.scss';
 
 /** @type {React.FC} */
 export const Court = () => {
+  useEffect(() => {
+    const onScreenChange = () => {
+      const bodyStyle = document.body.style;
+      const width = Math.round(0.8 * document.body.clientWidth);
+      const height = Math.round(315 / 680 * width);
+      bodyStyle.setProperty('--court_width', `${width}px`);
+      bodyStyle.setProperty('--court_height', `${height}px`);
+    }
+
+    onScreenChange();
+    window.addEventListener('resize', onScreenChange);
+
+    return () => {
+      window.removeEventListener('resize', onScreenChange);
+    };
+  });
+
   return (
     <div className={styles.court}>
       <HalfCourt className={styles.left_half}>
